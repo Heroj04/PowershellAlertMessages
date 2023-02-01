b5cf90c64c937ffbc272841690591611a6cfd4f3
Jack Footner <jackrfootner@gmail.com>
2023-02-01 11:42:38 +1030

    Computer Refresh Timeout

--- MessageWatcher.ps1
+++ MessageWatcher.ps1
@@ -2,29 +2,38 @@
 $computers = @()
 $groups = @()
 $ous = @()
-
-## File Watcher
 $Path = "C:\MessageAlert\Messages"
 $HistoryPath = "C:\MessageAlert\MessageHistory"
 $Timeout = 1000
+$refreshTimeout = 900000
+
+$lastRefresh = Get-Date
+$allComputers = @()
 
 ### Expand Groups and OUs
-foreach ($groupName in $groups) {
-    $groupMembers = Get-ADGroupMember $groupName -Recursive | ForEach-Object {$_.Name}
-    foreach ($member in $groupMembers) {
-        if ($computers -notcontains $member) {
-            $computers += $member
+function refreshAllComputers {
+    $return = @()
+    $return += $computers
+    foreach ($groupName in $groups) {
+        $groupMembers = Get-ADGroupMember $groupName -Recursive | ForEach-Object {$_.Name}
+        foreach ($member in $groupMembers) {
+            if ($return -notcontains $member) {
+                $return += $member
+            }
         }
     }
-}
-foreach ($ouString in $ous) {
-    $ouMembers = Get-ADComputer -Filter * -SearchBase $ouString | ForEach-Object {$_.Name}
-    foreach ($member in $ouMembers) {
-        if ($computers -notcontains $member) {
-            $computers += $member
+    foreach ($ouString in $ous) {
+        $ouMembers = Get-ADComputer -Filter * -SearchBase $ouString | ForEach-Object {$_.Name}
+        foreach ($member in $ouMembers) {
+            if ($return -notcontains $member) {
+                $return += $member
+            }
         }
     }
+    Write-Host "Refreshed Computer List" -BackgroundColor Black -ForegroundColor White
+    return $return
 }
+$allComputers = refreshAllComputers
 
 ### DEFINE ACTIONS AFTER AN EVENT IS DETECTED
 function action {
@@ -35,9 +44,10 @@ function action {
         $changedFilePath
     )
     $content = Get-Content -Path $changedFilePath -Raw
+    Write-Output "Processing Message ($changedFilePath):`n$content"
 
     ## Send Message to each computer
-    foreach ($computerName in $computers) {
+    foreach ($computerName in $allComputers) {
         if (test-connection $computerName -quiet -Count 1) {
             msg.exe * /SERVER:$computerName "$content"
             #Start-Process "msg" -ArgumentList "* /SERVER:$computerName `"$content`""
@@ -59,7 +69,12 @@ try {
     # start monitoring manually in a loop:
     do {
 
-        Start-Sleep $Timeout
+        Start-Sleep -Milliseconds $Timeout
+
+        if ($lastRefresh.AddMilliseconds($refreshTimeout) -lt (Get-Date)) {
+            $allComputers = refreshAllComputers
+            $lastRefresh = Get-Date
+        }
 
         $files = Get-ChildItem -Path $Path -Filter $FileFilter -File -Recurse
 
