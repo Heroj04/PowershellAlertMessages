ab6156bd1d0186fcba26032d43bb74c1fc86a67d
Jack Footner <jackrfootner@gmail.com>
2023-02-01 11:03:09 +1030

    Replace FileSystemWatcher with Get-ChildItem

--- MessageWatcher.ps1
+++ MessageWatcher.ps1
@@ -6,9 +6,6 @@ $ous = @()
 ## File Watcher
 $Path = "C:\MessageAlert\Messages"
 $HistoryPath = "C:\MessageAlert\MessageHistory"
-$IncludeSubfolders = $true
-$AttributeFilter = [IO.NotifyFilters]::FileName
-$ChangeTypes = [System.IO.WatcherChangeTypes]::Created
 $Timeout = 1000
 
 ### Expand Groups and OUs
@@ -34,14 +31,9 @@ function action {
     param
     (
         [Parameter(Mandatory)]
-        [System.IO.WaitForChangedResult]
-        $ChangeInformation
+        [String]
+        $changedFilePath
     )
-    Write-Warning 'Change detected:'
-    $ChangeInformation | Out-String | Write-Host -ForegroundColor DarkYellow
-
-    ## Read the message file
-    $changedFilePath = Join-Path $Path $ChangeInformation.Name
     $content = Get-Content -Path $changedFilePath -Raw
 
     ## Send Message to each computer
@@ -62,30 +54,22 @@ function action {
 
 ### SET Watcher
 try {
-    Write-Warning "FileSystemWatcher is monitoring $Path"
-    
-    # create a filesystemwatcher object
-    $watcher = New-Object -TypeName IO.FileSystemWatcher -ArgumentList $Path, $FileFilter -Property @{
-        IncludeSubdirectories = $IncludeSubfolders
-        NotifyFilter = $AttributeFilter
-    }
+    Write-Warning "Watcher is monitoring $Path"
 
     # start monitoring manually in a loop:
     do {
-        # wait for changes for the specified timeout
-        # IMPORTANT: while the watcher is active, PowerShell cannot be stopped
-        # so it is recommended to use a timeout of 1000ms and repeat the
-        # monitoring in a loop. This way, you have the chance to abort the
-        # script every second.
-        $result = $watcher.WaitForChanged($ChangeTypes, $Timeout)
-        # if there was a timeout, continue monitoring:
-        if ($result.TimedOut) { continue }
-        
-        action -Change $result
-        # the loop runs forever until you hit CTRL+C    
+
+        Start-Sleep $Timeout
+
+        $files = Get-ChildItem -Path $Path -Filter $FileFilter -File -Recurse
+
+        if ($files.Length -gt 0) {
+            foreach ($file in $files) {
+                action -changedFilePath $file.FullName
+            }
+        }
+
     } while ($true)
 } finally {
-    # release the watcher and free its memory:
-    $watcher.Dispose()
-    Write-Warning 'FileSystemWatcher removed.'
+    Write-Warning 'Watcher removed.'
 }
